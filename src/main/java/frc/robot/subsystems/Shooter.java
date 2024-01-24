8fc9708ee06a97d187be3b8a3ee84a05b2c218f1
Ricky Ward <rward0079@outlook.com>
2024-01-24 16:11:24 -0500

    shooter stuff

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems;
+
+import org.frc5587.lib.subsystems.SimpleMotorBase;
+
+public class Shooter extends SimpleMotorBase {
+    public Shooter() {
+        super()
+    }
+}
