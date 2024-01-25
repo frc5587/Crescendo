fb4a29ac859e33c19f14f604b6edb337cba9325f
Ricky Ward <rward0079@outlook.com>
2024-01-25 18:33:31 -0500

    added pathplanner vendordep

--- src/main/java/frc/robot/commands/AutoCommands.java
+++ src/main/java/frc/robot/commands/AutoCommands.java
@@ -1,5 +1,8 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.path.PathPlannerTrajectory;
+
 public class AutoCommands {
-    
+    // Auto Paths
+    private PathPlannerTrajectory amp_dep_preloaded;
 }
