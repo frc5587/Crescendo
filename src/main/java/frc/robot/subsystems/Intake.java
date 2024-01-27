50dcbb4656edcff2d26f1567f7a9d521bd684551
Sebi <sebastian.iov@outlook.com>
2024-01-26 23:40:50 -0500

    Intake Code

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,25 @@
+package frc.robot.subsystems;
+
+import org.frc5587.lib.subsystems.SimpleMotorBase;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import frc.robot.Constants.IntakeConstants;
+
+public class Intake extends SimpleMotorBase {
+    private static CANSparkMax Motor = new CANSparkMax(IntakeConstants.MOTOR_ID, MotorType.kBrushless);
+ 
+    
+    public Intake() {
+        super(Motor, IntakeConstants.FORWARD_THROTTLE, IntakeConstants.REVERSE_THROTTLE);
+    }
+
+    @Override
+    public void configureMotors() {
+        Motor.restoreFactoryDefaults();
+        Motor.setInverted(IntakeConstants.MOTOR_INVERTED);
+        Motor.setSmartCurrentLimit(IntakeConstants.STALL_LIMIT, IntakeConstants.FREE_LIMIT);
+       
+    }
+}
