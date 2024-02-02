113bc0d6b8302a7a85a5dc4271780f58207790f9
Hazel Belmont <hparkerbelmont@gmail.com>
2024-02-02 18:11:45 -0500

    format arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.ArmConstants;
@@ -15,57 +14,60 @@ import frc.robot.Constants.ArmConstants;
 public class Arm extends PivotingArmBase {
     private static TalonFX rightMotor = new TalonFX(ArmConstants.RIGHT_MOTOR_ID);
     private static TalonFX leftMotor = new TalonFX(ArmConstants.LEFT_MOTOR_ID);
- private final DutyCycleEncoder throughBore = new DutyCycleEncoder(0);
+    private final DutyCycleEncoder throughBore = new DutyCycleEncoder(0);
 
- public static PivotingArmConstants constants = new PivotingArmConstants (
-        ArmConstants.GEARING_MOTOR_TO_ARM,
-        1,
-        0,
-        ArmConstants.SOFT_LIMITS,
-        (int) ArmConstants.ZERO_OFFSET,
-        ArmConstants.ENCODER_CPR,
-        ArmConstants.PID,
-        ArmConstants.FF
-        
- );
+    public static PivotingArmConstants constants = new PivotingArmConstants(
+            ArmConstants.GEARING_MOTOR_TO_ARM,
+            1,
+            0,
+            ArmConstants.SOFT_LIMITS,
+            (int) ArmConstants.ZERO_OFFSET,
+            ArmConstants.ENCODER_CPR,
+            ArmConstants.PID,
+            ArmConstants.FF
 
- public Arm(TalonFX leftMotor, TalonFX rightMotor) {
-    super("arm", constants, leftMotor); 
-    enable();
-    resetEncoders();
-    setGoal(0);
-    throughBore.setDutyCycleRange(1.0/1024.0, 1023.0/1024.0); //placeholder values
+    );
 
- }
+    public Arm(TalonFX leftMotor, TalonFX rightMotor) {
+        super("arm", constants, leftMotor);
+        enable();
+        resetEncoders();
+        setGoal(0);
+        throughBore.setDutyCycleRange(1.0 / 1024.0, 1023.0 / 1024.0); // placeholder values
 
-@Override
-public double getEncoderPosition() {
-    return leftMotor.getPosition().getValueAsDouble(); 
-}
+    }
 
-@Override
-public double getEncoderVelocity() {
-    return leftMotor.getVelocity().getValueAsDouble();
-}
+    @Override
+    public double getEncoderPosition() {
+        return leftMotor.getPosition().getValueAsDouble();
+    }
 
-@Override
-public void setEncoderPosition(double position) {
-    leftMotor.setPosition(position);
-}
+    @Override
+    public double getEncoderVelocity() {
+        return leftMotor.getVelocity().getValueAsDouble();
+    }
 
-@Override
-public void configureMotors() {
-    leftMotor.setNeutralMode(NeutralModeValue.Brake);
-    rightMotor.setNeutralMode(NeutralModeValue.Brake);
+    @Override
+    public void setEncoderPosition(double position) {
+        leftMotor.setPosition(position);
+    }
 
-    leftMotor.setInverted(true);
-    rightMotor.setInverted(false);
+    @Override
+    public void configureMotors() {
+        leftMotor.setNeutralMode(NeutralModeValue.Brake);
+        rightMotor.setNeutralMode(NeutralModeValue.Brake);
 
-    leftMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(ArmConstants.STALL_LIMIT).withSupplyCurrentLimit(ArmConstants.FREE_LIMIT));
-    rightMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(ArmConstants.STALL_LIMIT).withSupplyCurrentLimit(ArmConstants.FREE_LIMIT));
+        leftMotor.setInverted(true);
+        rightMotor.setInverted(false);
 
-    rightMotor.setControl(new Follower(leftMotor.getDeviceID(), ArmConstants.LEFT_MOTOR_INVERTED != ArmConstants.RIGHT_MOTOR_INVERTED));
-}
+        leftMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(ArmConstants.STALL_LIMIT)
+                .withSupplyCurrentLimit(ArmConstants.FREE_LIMIT));
+        rightMotor.getConfigurator().apply(new CurrentLimitsConfigs().withStatorCurrentLimit(ArmConstants.STALL_LIMIT)
+                .withSupplyCurrentLimit(ArmConstants.FREE_LIMIT));
+
+        rightMotor.setControl(new Follower(leftMotor.getDeviceID(),
+                ArmConstants.LEFT_MOTOR_INVERTED != ArmConstants.RIGHT_MOTOR_INVERTED));
+    }
 
     public void ArmSpeaker() {
         setGoal(ArmConstants.SPEAKER_SETPOINT);
@@ -79,25 +81,25 @@ public void configureMotors() {
         setGoal(ArmConstants.RESTING_SETPOINT);
     }
 
- @Override
+    @Override
     public void periodic() {
         super.periodic();
-        if(SmartDashboard.getBoolean("Reset Encoders", false)) {
+        if (SmartDashboard.getBoolean("Reset Encoders", false)) {
             resetEncoders();
         }
         SmartDashboard.putBoolean("Reset Encoders", false);
 
-        if(!throughBore.isConnected()) {
+        if (!throughBore.isConnected()) {
             this.disable();
-         this.stop();
-     }
+            this.stop();
+        }
     }
 
-    public double getRawAbsolutePosition(){
+    public double getRawAbsolutePosition() {
         return throughBore.get();
     }
 
-    public void zeroThroughBore(){
+    public void zeroThroughBore() {
         double absolutePosition = getRawAbsolutePosition();
         throughBore.setPositionOffset(absolutePosition);
     }
