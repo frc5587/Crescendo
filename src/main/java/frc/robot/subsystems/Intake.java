8f7f8d74cc6032bfc2e78c7058e4b01562b641af
Ricky Ward <rward0079@outlook.com>
2024-02-13 16:18:49 -0500

    added smartdashboard value

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,17 +3,25 @@ package frc.robot.subsystems;
 import org.frc5587.lib.subsystems.SimpleMotorBase;
 
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.ColorSensorV3;
 
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.ShooterConstants;
 
 public class Intake extends SimpleMotorBase {
     private static CANSparkMax motor = new CANSparkMax(IntakeConstants.MOTOR_ID, MotorType.kBrushless);
- 
+    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 colorSensor = new ColorSensorV3(i2cPort);
     
     public Intake() {
         super(motor, ShooterConstants.FORWARD_THROTTLE, ShooterConstants.REVERSE_THROTTLE);
+
+        SmartDashboard.putNumber("Color Measurement Proximity", colorSensor.getProximity());
     }
 
     @Override
