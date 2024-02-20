9b1fdcde3bcd6a0ffa415d8a790833b11133cc61
Hazel Belmont <hparkerbelmont@gmail.com>
2024-02-20 14:44:08 -0500

    uhhh

--- src/main/java/frc/robot/commands/SimLineUpToSpeaker.java
+++ src/main/java/frc/robot/commands/SimLineUpToSpeaker.java
@@ -2,10 +2,9 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.FieldConstants;
 import frc.robot.subsystems.SimSwerve;
@@ -27,87 +26,102 @@ public class SimLineUpToSpeaker extends Command {
     @Override
     public void execute() {
         Pose2d currentPose = swerve.getPose();
-            if (DriverStation.getAlliance().get().equals(Alliance.Blue)) {
-                Rotation2d targetAngle = Rotation2d.fromRadians(Math.atan2(
-                        currentPose.getY() - FieldConstants.BLUE_SPEAKER_OPENING_TRANSLATION.getY(),
-                        currentPose.getX() - FieldConstants.BLUE_SPEAKER_OPENING_TRANSLATION.getX()));
-                if (currentPose.getRotation().getDegrees() > targetAngle.getDegrees()) {
-                    swerve.runVelocity(new ChassisSpeeds(0, 0, -1));
-                } else {
-                    swerve.runVelocity(new ChassisSpeeds(0, 0, 1));
-                }
-                if (Math.abs(currentPose.getRotation().getDegrees() - targetAngle.getDegrees()) < 2) {
-                    rotatingDone = true;
-                }
-
-                if ((Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) > 0.1)
-                        && (Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) > 0.1)) {
-                    swerve.runVelocity(new ChassisSpeeds(
+        if (DriverStation.getAlliance().get().equals(Alliance.Blue)) {
+            Rotation2d targetAngle = Rotation2d.fromRadians(Math.atan2(
+                    currentPose.getY() - FieldConstants.BLUE_SPEAKER_OPENING_TRANSLATION.getY(),
+                    currentPose.getX() - FieldConstants.BLUE_SPEAKER_OPENING_TRANSLATION.getX()));
+                    System.out.println("TARGET " + targetAngle.getDegrees());
+                    System.out.println("CURRENT " + currentPose.getRotation().getDegrees());
+            if (Math.abs(currentPose.getRotation().getDegrees() - targetAngle.getDegrees()) < 2) {
+                rotatingDone = true;
+            }
+            else {
+                rotatingDone = false;
+            }
+            if (Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) < 0.1
+                        && Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) < 0.1) {
+                    strafingDone = true;
+            }
+            else {
+                strafingDone = false;
+            }
+            if ((Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) > 0.1)
+                    && (Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) > 0.1)) {
+                swerve.drive(new Translation2d(
+                        -Math.copySign(1., currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()),
+                        -Math.copySign(1., currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY())),
+                        rotatingDone ? 0 : -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                        true, true);
+            }
+                else if (Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) > 0.1) {
+                    swerve.drive(new Translation2d(
                             -Math.copySign(1., currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()),
-                            -Math.copySign(1., currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()),
-                            0));
-                } else {
-                    if (Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) > 0.1) {
-                        swerve.runVelocity(
-                                new ChassisSpeeds(
-                                        -Math.copySign(1.,
-                                                currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()),
-                                        0, 0));
-                    }
-                    if (Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) > 0.1) {
-                        swerve.runVelocity(
-                                new ChassisSpeeds(0,
-                                        -Math.copySign(1.,
-                                                currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()),
-                                        0));
-                    }
-                    if (Math.abs(currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getX()) < 0.1
-                            && Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) < 0.1) {
-                        strafingDone = true;
-                    }
+                            0),
+                            rotatingDone ? 0 : -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
+                }
+                else if (Math.abs(currentPose.getY() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY()) > 0.1) {
+                    swerve.drive(new Translation2d(0, -Math.copySign(1.,
+                            currentPose.getX() - FieldConstants.BLUE_SUBWOOFER_FRONT_POSE.getY())),
+                            rotatingDone ? 0 : -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
+                }
+                else if(!rotatingDone) {
+                    swerve.drive(new Translation2d(0, 0),
+                            -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
                 }
             }
-            else if (DriverStation.getAlliance().get().equals(Alliance.Red)) {
-                Rotation2d targetAngle = Rotation2d.fromRadians(Math.atan2(
-                        currentPose.getY() - FieldConstants.RED_SPEAKER_OPENING_TRANSLATION.getY(),
-                        currentPose.getX() - FieldConstants.RED_SPEAKER_OPENING_TRANSLATION.getX()));
-                if (currentPose.getRotation().getDegrees() > targetAngle.getDegrees()) {
-                    swerve.runVelocity(new ChassisSpeeds(0, 0, -1));
-                } else {
-                    swerve.runVelocity(new ChassisSpeeds(0, 0, 1));
+
+        else if (DriverStation.getAlliance().get().equals(Alliance.Red)) {
+            Rotation2d targetAngle = Rotation2d.fromRadians(Math.atan2(
+                    currentPose.getY() - FieldConstants.RED_SPEAKER_OPENING_TRANSLATION.getY(),
+                    currentPose.getX() - FieldConstants.RED_SPEAKER_OPENING_TRANSLATION.getX()));
+                    System.out.println("TARGET " + targetAngle.getDegrees());
+                    System.out.println("CURRENT " + currentPose.getRotation().getDegrees());
+            if (Math.abs(currentPose.getRotation().getDegrees() - targetAngle.getDegrees()) < 2) {
+                rotatingDone = true;
+            }
+            else {
+                rotatingDone = false;
+            }
+            if (Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) < 0.1
+                        && Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) < 0.1) {
+                    strafingDone = true;
                 }
-                if (Math.abs(currentPose.getRotation().getDegrees() - targetAngle.getDegrees()) < 2) {
-                    rotatingDone = true;
+                else {
+                    strafingDone = false;
                 }
-                if ((Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) > 0.1)
-                        && (Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) > 0.1)) {
-                    swerve.runVelocity(new ChassisSpeeds(
-                            -Math.copySign(1., currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()),
-                            -Math.copySign(1., currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()),
-                            0));
-                } else {
-                    if (Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) > 0.1) {
-                        swerve.runVelocity(
-                                new ChassisSpeeds(
-                                        -Math.copySign(1.,
-                                                currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()),
-                                        0, 0));
-                    }
-                    if (Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) > 0.1) {
-                        swerve.runVelocity(
-                                new ChassisSpeeds(0,
-                                        -Math.copySign(1.,
-                                                currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()),
-                                        0));
-                    }
-                    if (Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) < 0.1
-                            && Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) < 0.1) {
-                        strafingDone = true;
-                    }
+            if ((Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) > 0.1)
+                    && (Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) > 0.1)) {
+                swerve.drive(new Translation2d(
+                        -Math.copySign(1., currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()),
+                        -Math.copySign(1., currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY())),
+                        -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                        true, true);
+            }
+                else if (Math.abs(currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()) > 0.1) {
+                    swerve.drive(new Translation2d(
+                            -Math.copySign(1.,
+                                    currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getX()),
+                            0),
+                            -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
+                }
+                else if (Math.abs(currentPose.getY() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY()) > 0.1) {
+                    swerve.drive(new Translation2d(0, -Math.copySign(1.,
+                            currentPose.getX() - FieldConstants.RED_SUBWOOFER_FRONT_POSE.getY())),
+                            -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
+                }
+                else if(!rotatingDone) {
+                    swerve.drive(new Translation2d(0, 0),
+                            -Math.copySign(1., currentPose.getRotation().getDegrees() - targetAngle.getDegrees()),
+                            true, true);
+                }
             }
         }
-
-    }
+    
 
     @Override
     public boolean isFinished() {
